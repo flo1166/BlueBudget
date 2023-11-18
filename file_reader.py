da61c9db60fd8ec06e6bc8056cbfdd67e649bb07
flo1166 <floriankorn93@web.de>
2023-11-18 11:59:22 +0100

    added file reader, map and data sources

--- file_reader.py
+++ file_reader.py
@@ -23,4 +23,5 @@ def reader_groundwater():
     return df
 
 
+
         
---
+++ map.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 18 11:57:48 2023
+
+@author: Florian Korn
+"""
+
+import geopandas as gpd
