73385820f3630d18ed27b8272de3f81d21ec0d63
flo1166 <floriankorn93@web.de>
2023-11-18 18:23:25 +0100

    Added mapping and data for groundwater

--- file_reader.py
+++ file_reader.py
@@ -11,17 +11,17 @@ import pandas as pd
 
 os.chdir('C:/Eigene Dateien/HackaTumProjekt')
 
-def reader_groundwater():
-    df_names = os.listdir('src/groundwater')
+def reader_groundwater(directory):
+    df_names = os.listdir(directory)
     df_names = list(filter(re.compile(r"16*").match, df_names))
     df = pd.DataFrame()
     
     for i in df_names:    
-        temp_df = pd.read_csv('src/groundwater/' + i, sep = ';', header = 6, decimal = ',', parse_dates=['Datum'])
+        temp_df = pd.read_csv(directory + i, sep = ';', header = 6, decimal = ',', parse_dates=['Datum'])
         temp_df['messstation'] = i[:5]
         df = pd.concat([df, temp_df])
     return df
 
-
+df_groundwater = reader_groundwater('src/groundwater/')
 
         
--- map.py
+++ map.py
@@ -4,5 +4,23 @@ Created on Sat Nov 18 11:57:48 2023
 
 @author: Florian Korn
 """
-
+import os
+os.chdir('C:/Eigene Dateien/HackaTumProjekt')
 import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely.geometry import Point, Polygon
+
+plt.rcParams['figure.figsize'] = (20, 10)
+df_municipalities = gpd.read_file('src/cityborders/municipalities.json')
+mapping_groundwater = pd.read_csv('src/mapping/groundwater_mapping.csv', sep = ';')
+
+def map_groundwater_to_municipalities(df_municipalities, mapping_groundwater):
+    for l, i in enumerate(df_municipalities['geometry']):
+        for j in range(len(mapping_groundwater['east'])):
+            if i.contains(Point(mapping_groundwater.loc[j, 'east'], mapping_groundwater.loc[j, 'north'])):
+                mapping_groundwater.loc[j, 'municipality'] = df_municipalities.loc[l, 'name']
+    mapping_groundwater.to_csv('src/mapping/mapping_groundwater_complete.csv')
+    return mapping_groundwater
+
+mapping_groundwater = map_groundwater_to_municipalities(df_municipalities, mapping_groundwater)
